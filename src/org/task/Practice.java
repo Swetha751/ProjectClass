44cb5df2567e74d67ad8bdd87bf339ab2a1340b1
Swetha751 <swetharavi751@gmail.com>
2019-10-17 08:53:28 +0530

    added

--- src/org/task/Practice.java
+++ src/org/task/Practice.java
@@ -8,7 +8,11 @@ public class Practice extends Coding {
 	public void task1() {
 	System.out.println("Question 2");
 
+
 }
+	public static void main(String[] args) {
+		System.out.println(5);
+	}
 
 
 }
